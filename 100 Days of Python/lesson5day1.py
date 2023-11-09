74d8650f0c327acaec2ffa2e93afb807f90ceabb
GiorGianaCode <148870207+GiorGianaCode@users.noreply.github.com>
2023-11-09 20:12:44 +0100

    A few lessons

---
+++ 100 Days of Python/lesson5day1.py
@@ -0,0 +1,10 @@
+two_digit_number = input()
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+two_digit_number = str(two_digit_number)
+
+dig1 = two_digit_number[0]
+dig2 = two_digit_number[1]
+
+print(int(dig1) + int(dig2))
